ee9a69727995584503912dd22a1dca444e40bfa2
trishmarie002-art <trishmarie002@gmail.com>
2026-04-08 03:37:24 -0500

    Add sitemap function for website URLs

---
+++ app/sitemap.ts
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://jayslandclearingservicesanddirtwork.com',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://jayslandclearingservicesanddirtwork.com/contact',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://jayslandclearingservicesanddirtwork.com/services',
+      lastModified: new Date(),
+    },
+  ];
+}
