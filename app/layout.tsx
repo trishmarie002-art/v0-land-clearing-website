71c043fad523eb145cf033add650fb312f8a0f76
trishmarie002-art <trishmarie002@gmail.com>
2026-04-08 01:06:10 -0500

    Add Clarity tracking script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,11 @@
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "w8bxwftdzs");
+</script>
+
 import type { Metadata } from 'next'
 import { Oswald, Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
