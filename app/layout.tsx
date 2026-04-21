5da5f650179bee13f69f5b46f3390e0b70613611
trishmarie002-art <trishmarie002@gmail.com>
2026-04-21 17:57:32 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Oswald, Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
-import { GoogleTagManager } from '@next/third-parties/google'
 import Script from 'next/script'
 import './globals.css'
 
@@ -136,8 +135,27 @@ export default function RootLayout({
       </head>
 
       <body className={`${inter.variable} ${oswald.variable} font-sans antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MWD3RQLX"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         {children}
         <Analytics />
+
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MWD3RQLX');
+          `}
+        </Script>
 
         <Script id="microsoft-clarity" strategy="afterInteractive">
           {`
@@ -149,8 +167,6 @@ export default function RootLayout({
           `}
         </Script>
       </body>
-
-      <GoogleTagManager gtmId="GTM-MWD3RQLX" />
     </html>
   )
 }
