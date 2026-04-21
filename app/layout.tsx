cdb6d5422d0c639d4771bdcd234b752a9a74f6de
trishmarie002-art <trishmarie002@gmail.com>
2026-04-21 17:06:20 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -121,6 +121,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TBGQ2LPL');</script>
+<!-- End Google Tag Manager -->
+        
         {/* GTM noscript fallback */}
         <noscript>
           <iframe
