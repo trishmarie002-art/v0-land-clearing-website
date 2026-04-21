1b924a689416bccb3f4a4f7760a575f7adedabc9
trishmarie002-art <trishmarie002@gmail.com>
2026-04-21 17:38:07 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -121,13 +121,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TBGQ2LPL');</script>
-</* End Google Tag Manager */>
         
         {/* GTM noscript fallback */}
         <noscript>
@@ -153,10 +146,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       </head>
       
       <body> 
-        {/* Google Tag Manager (noscript) */}
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBGQ2LPL"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-{/* End Google Tag Manager (noscript) */}
         
         className={`${inter.variable} ${oswald.variable} font-sans antialiased`}>
         {children}
